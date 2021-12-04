bac141898ea97c5c2a9570c0d72795526f10b7d4
kyuwon53 <lkw2v@naver.com>
2021-12-05 02:45:29 +0900

    [Add] GET 요청을 받아라!

--- app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -3,15 +3,32 @@ package com.codesoom.assignment;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 
 public class DemoHttpHandler implements HttpHandler {
     @Override
-    public void handle(HttpExchange httpExchange) {
+    public void handle(HttpExchange httpExchange) throws IOException {
         String method = httpExchange.getRequestMethod();
         URI uri = httpExchange.getRequestURI();
         String path = uri.getPath();
 
         System.out.println(method + " " + path);
+
+        String content = "[]";
+
+        if(path.equals("/tasks")){
+            if(method.equals("GET")){
+                httpExchange.sendResponseHeaders(200, content.getBytes().length);
+
+                OutputStream outputStream = httpExchange.getResponseBody();
+
+                outputStream.write(content.getBytes());
+                outputStream.flush();
+                outputStream.close();
+            }
+        }
     }
 }

6284d3576ff097db39bf64af0c2e7e39e0bb4bfe
kyuwon53 <lkw2v@naver.com>
2021-12-05 03:21:46 +0900

    [Add] GET 요청을 받아서 출력하라

--- app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -6,7 +6,6 @@ import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URI;
-import java.nio.charset.StandardCharsets;
 
 public class DemoHttpHandler implements HttpHandler {
     @Override

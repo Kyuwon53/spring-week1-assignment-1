4e7f0c20c89f400729474d6e0b705c3161835555
kyuwon53 <lkw2v@naver.com>
2021-12-05 11:55:32 +0900

    [Add] POST 요청시 ToDo를 생성하라

--- app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -1,13 +1,22 @@
 package com.codesoom.assignment;
 
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.checkerframework.checker.units.qual.A;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class DemoHttpHandler implements HttpHandler {
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private List<Task> tasks = new ArrayList<>();
+    private Long newId = 0L;
+
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
         String method = httpExchange.getRequestMethod();
@@ -18,16 +27,42 @@ public class DemoHttpHandler implements HttpHandler {
 
         String content = "[]";
 
-        if(path.equals("/tasks")){
-            if(method.equals("GET")){
-                httpExchange.sendResponseHeaders(200, content.getBytes().length);
+        if(path.equals("/tasks")) {
+            if (method.equals("GET")) {
+                content = "GET list";
+            }
+            if(method.equals("POST")){
+                InputStream inputStream = httpExchange.getRequestBody();
+                String body = new BufferedReader(new InputStreamReader(inputStream))
+                        .lines()
+                        .collect(Collectors.joining("\n"));
 
-                OutputStream outputStream = httpExchange.getResponseBody();
 
-                outputStream.write(content.getBytes());
-                outputStream.flush();
-                outputStream.close();
+
+                Task task = objectMapper.readValue(body, Task.class);
+
+                newId += 1;
+                task.setId(newId);
+                tasks.add(task);
+
+                content = toJSON(tasks);
+
             }
         }
+
+        httpExchange.sendResponseHeaders(200, content.getBytes().length);
+
+        OutputStream outputStream = httpExchange.getResponseBody();
+
+        outputStream.write(content.getBytes());
+        outputStream.flush();
+        outputStream.close();
+    }
+
+    private String toJSON(Object object) throws IOException {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, object);
+
+        return outputStream.toString();
     }
 }
